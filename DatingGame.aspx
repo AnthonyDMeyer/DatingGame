cb17fd223f6833149e4ac44983b662cdcd25d3ce
AnthonyDMeyer <71147845+AnthonyDMeyer@users.noreply.github.com>
2020-09-24 10:48:15 -0500

    Add project files.

---
+++ DatingGame.aspx
@@ -0,0 +1,34 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="DatingGame.aspx.cs" Inherits="DatingGame.DatingGame" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Welcome to the Dating Game!!!<br />
+            <br />
+            <asp:Label ID="Label1" runat="server" Text="Name:"></asp:Label>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Go" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="Read" />
+            <br />
+            <asp:TextBox ID="TextBox1" runat="server" Width="212px"></asp:TextBox>
+            <br />
+            <asp:Label ID="Label2" runat="server" Text="Question:"></asp:Label>
+            <br />
+            <asp:TextBox ID="TextBox2" runat="server" Width="212px"></asp:TextBox>
+            <br />
+            <asp:Label ID="Label3" runat="server" Text="Question-Response:"></asp:Label>
+            <br />
+            <asp:TextBox ID="TextBox3" runat="server" Width="400px"></asp:TextBox>
+            <br />
+            <asp:ListBox ID="ListBox1" runat="server" Height="300px" Width="400px"></asp:ListBox>
+        </div>
+    </form>
+</body>
+</html>
---
+++ DatingGame.aspx.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+
+
+namespace DatingGame
+{
+    public partial class DatingGame : System.Web.UI.Page
+    {
+        string name;
+
+        static string fPath = @"C:\Users\Zoltan\Documents\aaa_School\Operating Systems\homework120\datinggame1";
+        static FileStream fs = new FileStream(fPath, FileMode.Create);
+        static StreamWriter writer = new StreamWriter(fs);
+        
+        
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            name = TextBox1.Text;
+
+            ListBox1.Items.Add(name + ": " + TextBox2.Text + TextBox3.Text);
+            writer.WriteLine(name + ": " + TextBox2.Text + TextBox3.Text);
+            writer.Close();
+
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            ListBox1.Items.Clear();
+            const string sPath = @"C:\Users\Zoltan\Documents\aaa_School\Operating Systems\homework120\datinggame1";
+            name = TextBox1.Text;
+            FileInfo fin = new FileInfo(sPath);
+
+            using (StreamReader stRead = fin.OpenText())
+            {
+                while (!stRead.EndOfStream)
+                {
+                    string s = stRead.ReadLine();
+                    ListBox1.Items.Add(s);
+                }
+                stRead.Close();
+            }
+
+        }
+    }
+}
